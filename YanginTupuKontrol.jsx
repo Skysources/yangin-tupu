d11545d39dc7bcd6a6153446fdc7fd1aeeb8647d
Skysources <115178647+Skysources@users.noreply.github.com>
2026-07-14 11:44:21 +0300

    Delete YanginTupuKontrol.jsx

--- YanginTupuKontrol.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import YanginTupuKontrol from "./YanginTupuKontrol.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <YanginTupuKontrol />
-  </React.StrictMode>
-);
